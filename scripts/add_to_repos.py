26ad1f1a23d28251fa9b1ea51053590088e736aa
viswanath <viswa.kolachina@gmail.com>
2024-10-09 20:32:06 -0500

    This is my second  commit

--- scripts/add_to_repos.py
+++ scripts/add_to_repos.py
@@ -9,10 +9,10 @@ import csv
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 GITHUB_API_URL = "https://api.github.com"
-TOKEN = os.getenv('GITHUB_TOKEN')
+TOKEN = os.getenv('PERSONAL_ACCESS_TOKEN')
 
 if not TOKEN:
-    logging.error("GITHUB_TOKEN not found. Please set the GITHUB_TOKEN environment variable.")
+    logging.error("PERSONAL_ACCESS_TOKEN not found. Please set the PERSONAL_ACCESS_TOKEN environment variable.")
     sys.exit(1)
 
 def validate_input(username, repo, permission):
